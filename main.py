d3022796a719702dab4c59b59c1f21d6b0b20d3e
Yurilord <Fritz-h.mann@protonmail.com>
2026-05-10 19:49:14 +0200

    added call_function functionality, some refactoring of main

--- main.py
+++ main.py
@@ -1,51 +1,91 @@
 import os
 import argparse
+import sys
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
+from config import MAX_ITERATIONS
 
 def main():
-    #Gemini API
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    if api_key is None:
-        raise RuntimeError("API key not found")
-    client = genai.Client(api_key=api_key)
-
     #argparse to handle command-line arguments
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
-    #Local model
+    #Gemini API
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("API key not found")
+    
+    #Client
+    client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f" User prompt: {args.user_prompt}")
+
+    #Chat loop
+    for _ in range(MAX_ITERATIONS):
+        try:
+            final_response = generate_content(client, messages, args.verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                return
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+
+    print(f"Maximum iterations ({MAX_ITERATIONS}) reached")
+    sys.exit(1)
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash", 
         contents=messages,
         config=types.GenerateContentConfig(
             system_instruction=system_prompt, 
             tools =[available_functions]
-            ),
-        )
+        ),
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response malformed")
 
-    if args.verbose:
-        if response.usage_metadata is not None:
-            print(f" User prompt: {args.user_prompt}")
-            print(f" Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f" Response tokens: {response.usage_metadata.candidates_token_count}")
-        else:
-            raise RuntimeError("No usuage metadata. Failed API request")
+    if verbose:     
+        print(f" Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f" Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if response.candidates:
+        for candidate in response.candidates:
+            if candidate.content:
+                messages.append(candidate.content)
+
+    if not response.function_calls:
+        return response.text    
     
-    if response.function_calls != None:
-        for item in response.function_calls:
-            print(f"Calling function: {item.name}({item.args})")
-    else:
+    
+    if not response.function_calls:
         print("Response:")
         print(response.text)
+        return
+                
+    function_responses = []
+    for function_call in response.function_calls:
+        result = call_function(function_call, verbose)
+        if (
+            not result.parts
+            or not result.parts[0].function_response
+            or not result.parts[0].function_response.response
+        ):
+            raise RuntimeError(f"Empty function response for {function_call.name}")
+        if verbose:
+            print(f"-> {result.parts[0].function_response.response}")
+        function_responses.append(result.parts[0])
 
+    messages.append(types.Content(role="user", parts=function_responses))
 
 if __name__ == "__main__":
     main()
